0eab8950c8ccbd63193525138b12b4a221d70dc3
Colton Smith <lilcoltsmith@gmail.com>
2018-09-28 21:18:58 -0400

    Initial engine with capability to load/render 3D models in .obj format.

---
+++ src/entities/Camera.java
@@ -0,0 +1,46 @@
+package entities;
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.util.vector.Vector3f;
+
+public class Camera {
+	private Vector3f position = new Vector3f(0, 0, 0);
+	private float pitch; //rotate around x
+	private float yaw;   //rotate around y
+	private float roll;  //rotate around z
+	
+	public Camera() {}
+	
+	public void move() {
+		if(Keyboard.isKeyDown(Keyboard.KEY_W)){
+			position.z-=0.1f;
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_S)){
+			position.z+=0.1f;
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_D)){
+			position.x+=0.1f;
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_A)){
+			position.x-=0.1f;
+		}
+	}
+
+	public Vector3f getPosition() {
+		return position;
+	}
+
+	public float getPitch() {
+		return pitch;
+	}
+
+	public float getYaw() {
+		return yaw;
+	}
+
+	public float getRoll() {
+		return roll;
+	}
+	
+	
+}
